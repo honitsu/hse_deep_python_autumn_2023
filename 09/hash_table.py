f38c7f76017c1db97e2a875d41112ed9f0534537
PERFECTHEART <101324009+PERFECTHEART@users.noreply.github.com>
2023-12-24 12:10:15 +0300

    Add files via upload

--- 09/hash_table.py
+++ 09/hash_table.py
@@ -1,4 +1,6 @@
 CAPACITY = 20
+
+
 class HashTable:
     def __init__(self, initial_capacity=CAPACITY):
         self.capacity = initial_capacity
@@ -11,7 +13,7 @@ class HashTable:
     def rehash(self):
         new_capacity = self.capacity * 2
         new_buckets = [None] * new_capacity
-        for item in self.get_items():
+        for item in self.items():
             key, value = item
             index = hash(key) % new_capacity
             if new_buckets[index] is None:
@@ -19,6 +21,45 @@ class HashTable:
             new_buckets[index].append((key, value))
         self.capacity = new_capacity
         self.buckets = new_buckets
+
+    def get_capacity(self):
+        return self.capacity
+
+    def clear(self):
+        self.buckets = [None] * self.capacity
+        self.size = 0
+
+    def copy(self):
+        new_table = HashTable(self.capacity)
+        new_table.size = self.size
+        new_table.buckets = [bucket[:] if bucket is not None else None for bucket in self.buckets]  # pylint: disable=unsubscriptable-object
+        return new_table
+
+    @staticmethod
+    def fromkeys(sequence, value=None):
+        new_table = HashTable(len(sequence))
+        for key in sequence:
+            if new_table.get(key) is None:
+                new_table[key] = value
+        return new_table
+
+    def get(self, key, default=None):
+        index = self.hash(key)
+        if self.buckets[index] is not None:
+            for stored_key, value in self.buckets[index]:
+                if stored_key == key:
+                    return value
+        return default
+
+    def items(self):
+        return [(key, value) for bucket in self.buckets if bucket is not None for key, value in bucket]  # pylint: disable=not-an-iterable
+
+    def keys(self):
+        result = []
+        for bucket in self.buckets:
+            if bucket is not None:
+                result.extend([key for key, _ in bucket])  # pylint: disable=not-an-iterable
+        return result
 
     def __setitem__(self, key, value):
         index = self.hash(key)
@@ -42,49 +83,3 @@ class HashTable:
                 if stored_key == key:
                     return value
         raise ValueError("Key does not exist")
-
-    def get_item(self, key, default=None):
-        index = self.hash(key)
-        if self.buckets[index] is not None:
-            for stored_key, value in self.buckets[index]:
-                if stored_key == key:
-                    return value
-        return default
-
-    def clear(self):
-        self.buckets = [None] * self.capacity
-        self.size = 0
-
-    def get_capacity(self):
-        return self.capacity
-    def copy(self):
-        new_table = HashTable(self.capacity)
-        new_table.size = self.size
-        new_table.buckets = [bucket[:] if bucket is not None else None for bucket in self.buckets]  # pylint: disable=unsubscriptable-object
-        return new_table
-
-    @staticmethod
-    def fromkeys(sequence, value=None):
-        new_table = HashTable(len(sequence))
-        for key in sequence:
-            if new_table.get_item(key) is None:
-                new_table.__setitem__(key, value)
-        return new_table
-
-    def items(self):
-        result = []
-        for bucket in self.buckets:
-            if bucket is not None:
-                result.extend(bucket)
-        return result
-
-    def keys(self):
-        result = []
-        for bucket in self.buckets:
-            if bucket is not None:
-                result.extend([key for key, _ in bucket])  # pylint: disable=not-an-iterable
-
-        return result
-
-    def get_items(self):
-        return [(key, value) for bucket in self.buckets if bucket is not None for key, value in bucket]  # pylint: disable=not-an-iterable
--- 09/test_hashtable.py
+++ 09/test_hashtable.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
 import unittest
 from hash_table import HashTable
 
 
 CAPACITY = 20
+
+
 class TestHashTable(unittest.TestCase):
     def test_init(self):
         ht = HashTable()
@@ -18,62 +22,63 @@ class TestHashTable(unittest.TestCase):
 
     def test_rehash(self):
         ht = HashTable()
-        ht.__setitem__("apple", 1)
-        ht.__setitem__("pear", 2)
-        ht.__setitem__("peach", 3)
-        ht.__setitem__("peach", 4)
+        ht["apple"] = 1
+        ht["pear"] = 2
+        ht["peach"] = 3
+        ht["peach"] = 4
         ht.rehash()
-        self.assertEqual(ht.get_capacity(), CAPACITY*2)
+        self.assertEqual(ht.get_capacity(), CAPACITY * 2)
 
     def test_setitem_and_getitem(self):
         ht = HashTable()
-        ht.__setitem__("apple", 10)
-        self.assertEqual(ht.get_item("apple"), 10)
-
-    def test___getitem__(self):
-        ht = HashTable()
-        ht.__setitem__("apple", 10)
-        self.assertEqual(ht.__getitem__("apple"), 10)
-        with self.assertRaises(ValueError):
-            ht.__getitem__("orange")
+        ht["apple"] = 10
+        self.assertEqual(ht.get("apple"), 10)
 
     def test_getitem_default_value(self):
         ht = HashTable()
-        self.assertIsNone(ht.get_item("pear", default=None))
+        self.assertIsNone(ht.get("pear", default=None))
 
     def test_clear(self):
         ht = HashTable()
-        ht.__setitem__("apple", 10)
+        ht["apple"] = 10
         ht.clear()
-        self.assertIsNone(ht.get_item("apple", default=None))
+        self.assertIsNone(ht.get("apple", default=None))
 
     def test_copy(self):
         ht = HashTable()
-        ht.__setitem__("apple", 10)
+        ht["apple"] = 10
         ht_copy = ht.copy()
         self.assertEqual(ht.items(), ht_copy.items())
 
     def test_fromkeys(self):
         ht = HashTable()
         ht_fromkeys = ht.fromkeys(["mango", "peach"], 50)
-        self.assertEqual(ht_fromkeys.get_item("mango"), 50)
-        self.assertEqual(ht_fromkeys.get_item("peach"), 50)
+        self.assertEqual(ht_fromkeys.get("mango"), 50)
+        self.assertEqual(ht_fromkeys.get("peach"), 50)
 
     def test_keys(self):
         ht = HashTable()
-        ht.__setitem__("apple", 10)
-        ht.__setitem__("banana", 20)
+        ht["apple"] = 10
+        ht["banana"] = 20
         keys = ht.keys()
         self.assertListEqual(sorted(keys), ["apple", "banana"])
 
     def test_expand(self):
         ht = HashTable(initial_capacity=2)
         print(ht.size)
-        ht.__setitem__("apple", 10)
-        ht.__setitem__("banana", 20)
-        ht.__setitem__("orange", 30)
+        ht["apple"] = 10
+        ht["banana"] = 20
+        ht["orange"] = 30
         self.assertEqual(len(ht.items()), 3)
         self.assertEqual(ht.capacity, 4)
+        self.assertEqual(ht["apple"], 10)
+        self.assertEqual(ht["banana"], 20)
+        self.assertEqual(ht["orange"], 30)
+
+    def test_no_such_key(self):
+        ht = HashTable(initial_capacity=2)
+        with self.assertRaises(ValueError):
+            ht["melon"]  # pylint: disable=pointless-statement
 
 
 if __name__ == "__main__":
